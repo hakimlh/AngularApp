93eb395d82dd53a8f2a78296475d022c0deb9878
wiselh <wise.self.2@gmail.com>
2018-04-12 14:00:53 +0100

    testchanging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hakim! It\' Working =)';
+  title = 'Hakim! It\'s Working =)';
 }
